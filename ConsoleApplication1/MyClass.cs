63edf353c15fd52624ef3024c01c772af6f5bc48
JeffreyMJohnson <jefjohms@gmail.com>
2015-04-13 17:14:46 -0700

    finished intro to c sharp exercises.

---
+++ ConsoleApplication1/MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 1. Experiment with the class. Add more members, constructors. Turn it into a simple calculator 
+
+program that can add, subtract, multiply and divide.
+ */
+namespace ConsoleApplication1
+{
+    class MyClass
+    {
+        private int mResult;
+        public int Result
+        {
+            get { return mResult; }
+        }
+
+        public void ClearResult()
+        {
+            mResult = 0;
+        }
+
+        public MyClass Add(int rhs)
+        {
+            mResult += rhs;
+            return this;
+        }
+
+        public MyClass Sub(int rhs)
+        {
+            mResult -= rhs;
+            return this;
+        }
+
+        public static int SimpleAdditionFunction(int num1, int num2)
+        {
+           return num1 + num2;
+        }
+
+        public static int SimpleSubtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+    }
+
+    
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            int sum = MyClass.SimpleAdditionFunction(3,4);
+            myClass.Add(3).Add(4);
+
+            Console.WriteLine("3 + 4 = " + sum.ToString());
+            Console.WriteLine("3 + 4 = " + myClass.Result);
+
+            sum = MyClass.SimpleSubtraction(7, 5);
+            myClass.Sub(5);
+            Console.WriteLine("7 - 5 = " + sum.ToString());
+            Console.WriteLine("7 - 5 = " + myClass.Result);
+            Console.Read();
+        }
+    }
+}
